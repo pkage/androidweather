58d09acbd37fbd02568a1c8e6f84f8314bee08c9
pkage <pkage@mit.edu>
2015-09-21 08:04:50 -0400

    ignore .idea

---
+++ app/src/main/java/org/kagelabs/weather/Getter.java
@@ -0,0 +1,15 @@
+package org.kagelabs.weather;
+
+/**
+ * Created by Patrick on 9/20/15.
+ */
+
+
+
+public class Getter {
+
+    public String downloadData(String url) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        return "";
+    }
+}
